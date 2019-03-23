3dad616b17227ec8537d297173fb1c5dd0de652f
jeuxdictator <45318464+jeuxdictator@users.noreply.github.com>
2019-03-23 15:54:16 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.TOKEN);
+
+client.on("ready", () => {
+    console.log(`connecté : ${client.user.tag}!`)
+    client.user.setPresence({
+        game: { 
+            name: `les egns se co aux serveur !`,
+            type: 'WATCHING' 
+        },
+        status: 'dnd' 
+    })
+});
+
+
+
+client.on(`message`, message =>{
+    if(message.channel.id === "384285377709473792"){
+        if(message.content.startsWith(+ "j'accèpte")) {
+
+            message.delete()
+
+            let user = message.guild.member(message.author);
+
+            let role = message.guild.roles.find(m => m.id === "558992005301141530");
+            if(!role) return console.log("Le rôle n'existe pas !");
+
+            message.author.addRole(role).catch(console.error);
+            message.channel.send(`**Bravo, tu as accepté le règlement**`);
+            
+        }else{
+            message.delete()
+            message.author.send("Veuillez envoyé `j'accèpte` dans le salon, et non autre chose")
+        }
+    }
+
+});
